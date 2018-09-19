b70e65ed36bec9a88a7cb5dcfbc6603d76545230
tony-Ma-yami <tony.ma@yamibuy.com>
2018-09-19 15:57:44 +0800

    创建register-server

---
+++ src/main/java/com/tony/unit/RegisterApp.java
@@ -0,0 +1,17 @@
+package com.tony.unit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RegisterApp {
+    public static void main( String[] args ){
+        SpringApplication.run(RegisterApp.class,args);
+    }
+}
